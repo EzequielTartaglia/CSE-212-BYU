dafb0b204c24198d5dfee56ffe20884a3921f52e
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-01-04 14:21:17 -0300

    W1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,20 +1,23 @@
 public static class Arrays
 {
     /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.      
+    /// For example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples; // Return the array of multiples
     }
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -25,9 +28,23 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Early exit if no rotation is needed
+        if (data.Count == 0 || amount == 0)
+        {
+            return;
+        }
+
+        // Reduce amount to avoid unnecessary rotations
+        amount %= data.Count;
+
+        // Split the list into two parts: last 'amount' elements and the rest
+        var lastPart = data.GetRange(data.Count - amount, amount);
+        var firstPart = data.GetRange(0, data.Count - amount);
+
+        // Clear the original list and add the rotated elements
+        data.Clear();
+        data.AddRange(lastPart);
+        data.AddRange(firstPart);
     }
+
 }
